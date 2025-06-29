aa5c88c61efcb8361532f4848b8e03a4f3f64f15
Felix Burkhard <felix.burkhard@aitgmbh.de>
2025-06-29 20:13:49 +0200

    remove solution

--- ProjectManagerSimulatorApi/Program.cs
+++ ProjectManagerSimulatorApi/Program.cs
@@ -1,13 +1,10 @@
 using ProjectManagerSimulatorApi.Repositories;
-using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 builder.Services.AddSingleton<EstimateRepository>();
 builder.Services.AddSingleton<DeadlineRepository>();
 
@@ -16,12 +13,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/openapi/v1.json", "v1");
-        app.MapScalarApiReference();
-    });
 }
 
 app.UseHttpsRedirection();
