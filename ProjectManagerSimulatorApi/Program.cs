ea5bb84c66a34bc2df6f96a8a1e92f40bd22431f
Marcel Schmitz <aixellent@googlemail.com>
2025-06-30 10:41:28 +0200

    feat(openapi): add proj files and Pipeline

--- ProjectManagerSimulatorApi/Program.cs
+++ ProjectManagerSimulatorApi/Program.cs
@@ -3,6 +3,7 @@ using ProjectManagerSimulatorApi.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddOpenApi();
 
 builder.Services.AddControllers();
 builder.Services.AddSingleton<EstimateRepository>();
@@ -13,6 +14,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
